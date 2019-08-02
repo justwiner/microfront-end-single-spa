47459525c33ff0330ff2d8d80080087de8d98be6
justwiner <392110917@qq.com>
2019-08-02 17:13:40 +0800

    update

--- project2/vue.config.js
+++ project2/vue.config.js
@@ -1,16 +1,6 @@
 const webpack = require('webpack')
 const path = require('path');
 
-// module.exports = {
-//     entry: path.resolve(__dirname, 'src/project2.js'),
-//     output: {
-//         filename: 'project2.js',
-//         library: 'project2',
-//         libraryTarget: 'amd',
-//         path: path.resolve(__dirname, 'build/project2'),
-//     },
-// }
-
 module.exports = {
     chainWebpack: config => {
         config.entryPoints.clear()
